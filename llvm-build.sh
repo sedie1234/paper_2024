2f2f22fb81b13e5a50ba0a031452b8dc1673952e
suhwan <suhwan1717@gmail.com>
2024-01-05 13:18:35 +0900

    llvm onnx-mlir added

---
+++ llvm-build.sh
@@ -0,0 +1,12 @@
+mkdir llvm-project/build
+cd llvm-project/build
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_TARGETS_TO_BUILD="host" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DLLVM_ENABLE_RTTI=ON \
+   -DLLVM_ENABLE_LIBEDIT=OFF
+
+cmake --build . -- ${MAKEFLAGS}
+cmake --build . --target check-mlir
